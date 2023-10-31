e512f93e058ccd79caefe1b9ddc64d386237b3fc
Felipe Gomes <contato@felipe-gomes.com>
2023-10-31 12:46:20 -0300

    Fixing url path on production

--- src/App.js
+++ src/App.js
@@ -5,19 +5,18 @@ import Login from './components/Login'
 import Main from './components/Main';
 import Root from './components/Root.js';
 import {RouterProvider, createBrowserRouter, createRoutesFromElements, Route} from 'react-router-dom';
-const publicPath = process.env.PUBLIC_URL || '/'; 
+const publicPath = process.env.REACT_APP_PUBLIC_URL; 
 const appRouter = createBrowserRouter(createRoutesFromElements(
 <>
-  <Route path={publicPath} element={<Root/>}>  
+  <Route path='/' element={<Root/>}>  
     <Route index element={<Main ApiData={ApiData} isLoggedIn={isLoggedIn} publicPath={publicPath}/> } /> 
-    <Route path={`${publicPath}login`} element={<Login LogInSpotify={LogInSpotify} publicPath={publicPath} /> } />  
+    <Route path='login' element={<Login LogInSpotify={LogInSpotify} publicPath={publicPath} /> } />  
   </Route>
 </>
 ));
 
 
-export default function App() {  
-    console.log( `Public Path ${publicPath}`);   
+export default function App() {   
     return (
         <>      
           <RouterProvider router={appRouter}  />
